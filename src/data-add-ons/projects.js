2795463107b4e0bbec4bc7cec048f65396487918
snurby7 <ryan.bruns7@gmail.com>
2019-01-09 07:31:18 -0500

    initialize gatsby with theme

---
+++ src/data-add-ons/projects.js
@@ -0,0 +1,46 @@
+const projects = [
+  {
+    id: 'fafa8d8b-fe69-450c-9c98-9f1d5ed6dc67',
+    title: 'Insights',
+    link: '',
+    background: '',
+    displayText: `Insights is a React application for additional budget tracking needs I wanted that leverages data collected from You Need A Budget. Still currently a work in progress and this has been my morning coffee project. This is my moonshot project, so I'm not stopping on this one until it's done.`,
+  },
+  {
+    id: 'c647892c-9958-4551-9521-09921eb16983',
+    title: 'Yeoman',
+    link: '',
+    background: '',
+    displayText: '',
+  },
+  {
+    id: '9048c85e-5f2c-41c8-8a48-81025f802f0c',
+    title: 'Health Check',
+    link: '',
+    background: '',
+    displayText: '',
+  },
+  {
+    id: 'a8d49386-961f-4e1f-b8d5-af3b703d57e9',
+    title: 'Snippets',
+    link: '',
+    background: '',
+    displayText: `This one was a bit of a fun one to get started. I remember just watching a talk and seeing people type "ccc" and it looked like they had just created an elegant block of code in a snap. This made me think, I write a lot of code on a good day, a lot of this code could be centralized, not just for myself but as a team. So it started as just creating karma tests, not all developers love testing, but this was to add a little bit of gas to their engine and get that to a point of ease. It's still not perfect, but it's better than it was. From write test snippets it bloomed into writing typescript snippets for things we all used regularly and some names were starting to get rather long, so snippets were my answer to that problem.`,
+  },
+  {
+    id: '4048053f-9490-40c5-a160-c93e4f849aff',
+    title: 'Lazy Fields',
+    link: '',
+    background: '',
+    displayText: `Reduced the risks associated with copying and pasting code. Created a tool so I can just enter some information about a field, click a few buttons and out comes the latest code pattern to copy and paste. Hooray for less errors!`,
+  },
+  {
+    id: '1c7ff6e8-e1b8-4a89-aa15-29698f4d4db2',
+    title: 'Tool Box',
+    link: '',
+    background: '',
+    displayText: '',
+  }
+]
+
+export default projects
