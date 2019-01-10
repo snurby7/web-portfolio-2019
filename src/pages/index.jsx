4d00b8a05f82aa41dcf0e2cfc8c52fdc461f3355
snurby7 <ryan.bruns7@gmail.com>
2019-01-10 07:38:55 -0500

    replace some boilerplate

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -86,16 +86,15 @@ const Index = () => (
         <AboutHero>
           <Avatar src={avatar} alt="John Doe" />
           <AboutSub>
-            The English language can not fully capture the depth and complexity of my thoughts. So I'm incorporating
-            Emoji into my speech to better express myself. Winky face.
+            Sub Text Here
           </AboutSub>
         </AboutHero>
         <AboutDesc>
-          You know the way you feel when you see a picture of two otters holding hands? That's how you're gonna feel
-          every day. My mother cried the day I was born because she knew she’d never be prettier than me. You should
-          make me your campaign manager. I was born for politics. I have great hair and I love lying. Captain? The kids
-          want to know where Paulie the Pigeon is. I told them he got sucked up into an airplane engine, is that all
-          right?
+          About Description
+          About Description
+          About Description
+          About Description
+          About Description
         </AboutDesc>
       </About>
       <Contact>
