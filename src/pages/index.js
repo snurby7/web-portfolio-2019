4c05e88bffda35092f102cd75235d8bf66b16696
snurby7 <ryan.bruns7@gmail.com>
2019-02-03 09:40:26 -0500

    make the image smaller, center it!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,52 @@
-import React from 'react';
-import { graphql } from 'gatsby';
-import Hemlet from 'react-helmet';
-import Img from 'gatsby-image';
-import Layout from '../components/Layout';
+import React from 'react'
+import { graphql } from 'gatsby'
+import Hemlet from 'react-helmet'
+import Img from 'gatsby-image'
+import Layout from '../components/Layout'
 
-const SiteIndex = (props) => (
+const SiteIndex = props => (
   <Layout>
-        <Hemlet>
-          <title>{props.data.site.siteMetadata.title}</title>
-          <meta name="description" content={props.data.site.siteMetadata.description} />
-        </Hemlet>
-        <p>
-          Welcome! I'm Ryan, or as my coworkers like to call me <strong>"Buns"</strong>!
-          <br />
-          My little slice of the internet pie. I like to always learn something new. Which made me create this site, I
-          was sick of paying Wix for something I do for a living. Always looking for ways to optimize
-        </p>
-        <p>
-          I like to look at life as a journey, you never know where tomorrow will take you, but you can control today to set you up for a successful tomorrow.
-          When I'm not{' '}
-          <a href="https://media.giphy.com/media/o0vwzuFwCGAFO/giphy.gif">
-            tapping away at the keyboard
-          </a>{' '}
-          I like to do anything that doesn't have a screen. I've picked up
-          reading and I enjoy biking around!
-        </p>
-        <p>
-          Less, but better!
-        </p>
-        <Img fluid={props.data.ryan.childImageSharp.fluid} alt={props.data.site.siteMetadata.title} />
-      </Layout>
+    <Hemlet>
+      <title>{props.data.site.siteMetadata.title}</title>
+      <meta
+        name="description"
+        content={props.data.site.siteMetadata.description}
+      />
+    </Hemlet>
+    <p>
+      Welcome! I'm Ryan, or as my coworkers like to call me{' '}
+      <strong>"Buns"</strong>!
+      <br />
+      My little slice of the internet pie. I like to always learn something new.
+      Which made me create this site, I was sick of paying Wix for something I
+      do for a living. Always looking for ways to optimize
+    </p>
+    <p>
+      I like to look at life as a journey, you never know where tomorrow will
+      take you, but you can control today to set you up for a successful
+      tomorrow. When I'm not{' '}
+      <a href="https://media.giphy.com/media/o0vwzuFwCGAFO/giphy.gif">
+        tapping away at the keyboard
+      </a>{' '}
+      I like to do anything that doesn't have a screen. I've picked up reading
+      and I enjoy biking around!
+    </p>
+    <p>Less, but better!</p>
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+      }}
+    >
+      <Img
+        style={{ width: 500 }}
+        fluid={props.data.ryan.childImageSharp.fluid}
+        alt={props.data.site.siteMetadata.title}
+      />
+    </div>
+  </Layout>
 )
 
 export default SiteIndex
